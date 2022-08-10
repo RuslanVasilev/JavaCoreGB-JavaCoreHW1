16f45c4db14235deba36d60ad6ef7cbc9e2b96de
Руслан Васильев <104421314+RuslanVasilev@users.noreply.github.com>
2022-08-10 17:30:50 +0300

    Update Main.java
    Изменения имён

--- src/Homework1/Main.java
+++ src/Homework1/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         Team newTeam = new Team ("Тупарь",
-        new TeamMember("Вася", 5),
+        new TeamMember("Даша", 5),
         new TeamMember("Саша", 6),
-        new TeamMember("Коля", 6),
+        new TeamMember("Лёха", 6),
         new TeamMember("Дима", 7));
 
         Course course = new Course(new Cross(5), new Swimming(6), new Jump(4), new Flight(7));
