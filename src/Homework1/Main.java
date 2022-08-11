2357982cf70543323958f129ded8c5dd7bf97703
Руслан Васильев <104421314+RuslanVasilev@users.noreply.github.com>
2022-08-11 15:45:20 +0300

    Change name Миша

--- src/Homework1/Main.java
+++ src/Homework1/Main.java
@@ -9,12 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
         Team newTeam = new Team ("Тупарь",
-        new TeamMember("Даша", 5),
+        new TeamMember("Миша", 5),
         new TeamMember("Саша", 6),
         new TeamMember("Лёха", 6),
         new TeamMember("Дима", 7));
 
         Course course = new Course(new Cross(5), new Swimming(6), new Jump(4), new Flight(7));
         course.doIt(newTeam);
+        System.out.println();
     }
+
 }
